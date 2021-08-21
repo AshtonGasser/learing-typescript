af351401dbf3e1767e6143476ed6e8e716badd2d
Ashton Gasser <ashtongasser@gmail.com>
2021-08-20 19:17:59 -0500

    markers

--- parcel-maps-app/src/Company.ts
+++ parcel-maps-app/src/Company.ts
@@ -1,21 +1,27 @@
-import faker from 'faker'
+import faker from 'faker';
 
 export class Company {
-    companyName: string;
-    catchPhrase: string;
-    location: {
-        lat: number; // | string;
-        lng: number; // | string;
-      };
+  companyName: string;
+  catchPhrase: string;
+  location: {
+    lat: number; // | string;
+    lng: number; // | string;
+  };
 
-
-constructor() {
+  constructor() {
     this.companyName = faker.company.companyName();
     this.catchPhrase = faker.company.catchPhrase();
     this.location = {
-        lat: parseFloat(faker.address.latitude()),
-        lng: parseFloat(faker.address.longitude()),
-      };
-
-}
+      lat: parseFloat(faker.address.latitude()),
+      lng: parseFloat(faker.address.longitude()),
+    };
+  }
+  markerContent(): string {
+    return `
+    <div>
+    <h1>Company Name: ${this.companyName}</h1>
+    <h3>Catchphrase: ${this.catchPhrase}</h3>
+    </div>
+    `;
+  }
 }
--- parcel-maps-app/src/index.ts
+++ parcel-maps-app/src/index.ts
@@ -1,11 +1,13 @@
 //in terminal run: parcel index.html
- 
+ /// <reference types="@types/google.maps" />
 import { User } from './User'
 import { Company } from './Company'
-const user = new User()
-console.log(user);
+import { CustomMap } from './CustomMap'
 
+const customMap = new CustomMap('map');
 const company = new Company()
-console.log(company);
+const user = new User()
+ customMap.addMarker(user)
+ customMap.addMarker(company)
 
 
